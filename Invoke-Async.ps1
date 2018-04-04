3db7c9e99575b5a80d6b2d68547912b9cb7d808e
ɦɐɹɹў <ADM-BouweH01@antoniuszorggroep.local>
2018-04-04 12:01:08 +0200

    version 1.0.1

--- Invoke-Async.ps1
+++ Invoke-Async.ps1
@@ -1,4 +1,7 @@
 ﻿<#
+    version 1.0.1
+    converted $Threads to ArrayList
+
     version 1.0.0
     initial upload and moved synopsis inside function
 #>
@@ -45,97 +48,100 @@ param(
 #return performance data
 [switch]$Measure
 )
-Begin
-{
-
-    $Threads = @()
-    $Length = $JobsLeft = $Set.Length
-
-    $Count = 0
-    if($Length -lt $ThreadCount){$ThreadCount=$Length}
-    $timer = @(1..$ThreadCount  | ForEach-Object{$null})
-    $Jobs = @(1..$ThreadCount  | ForEach-Object{$null})
-    
-    If($PSCmdlet.ParameterSetName -eq 'cmdlet')
+    Begin
     {
-        $CmdType = (Get-Command $Cmdlet).CommandType
-        if($CmdType -eq 'Alias')
-        {
-            $CmdType = (Get-Command (Get-Command $Cmdlet).ResolvedCommandName).CommandType
-        }
+
+        [System.Collections.ArrayList]$Threads = @()
+        $Threads = @()
+        $Length = $JobsLeft = $Set.Length
+
+        $Count = 0
+        if($Length -lt $ThreadCount){$ThreadCount=$Length}
+        $timer = @(1..$ThreadCount  | ForEach-Object{$null})
+        $Jobs = @(1..$ThreadCount  | ForEach-Object{$null})
         
-        If($CmdType -eq 'Function')
+        If($PSCmdlet.ParameterSetName -eq 'cmdlet')
         {
-            $ScriptBlock = (Get-Item Function:\$Cmdlet).ScriptBlock
+            $CmdType = (Get-Command $Cmdlet).CommandType
+            if($CmdType -eq 'Alias')
+            {
+                $CmdType = (Get-Command (Get-Command $Cmdlet).ResolvedCommandName).CommandType
+            }
+            
+            If($CmdType -eq 'Function')
+            {
+                $ScriptBlock = (Get-Item Function:\$Cmdlet).ScriptBlock
+                1..$ThreadCount | ForEach-Object{ $Threads += [powershell]::Create().AddScript($ScriptBlock)}
+            }
+            ElseIf($CmdType -eq "Cmdlet")
+            {
+                1..$ThreadCount  | ForEach-Object{ $Threads += [powershell]::Create().AddCommand($Cmdlet)}
+            }
+        }
+        Else
+        {
             1..$ThreadCount | ForEach-Object{ $Threads += [powershell]::Create().AddScript($ScriptBlock)}
         }
-        ElseIf($CmdType -eq "Cmdlet")
-        {
-            1..$ThreadCount  | ForEach-Object{ $Threads += [powershell]::Create().AddCommand($Cmdlet)}
-        }
-    }
-    Else
-    {
-        1..$ThreadCount | ForEach-Object{ $Threads += [powershell]::Create().AddScript($ScriptBlock)}
+
+        If($Params){$Threads | ForEach-Object{$_.AddParameters($Params) | Out-Null}}
+
     }
 
-    If($Params){$Threads | ForEach-Object{$_.AddParameters($Params) | Out-Null}}
-
-}
-Process
-{
-    while($JobsLeft)
+    Process
     {
-        for($idx = 0; $idx -lt ($ThreadCount-1) ; $idx++)
+        while($JobsLeft)
         {
-            $SetParamObj = $Threads[$idx].Commands.Commands[0].Parameters| Where-Object {$_.Name -eq $SetParam}
-             
-            If($Jobs[$idx].IsCompleted) #job ran ok, clear it out
-            {  
-                $result = $null
-                if($threads[$idx].InvocationStateInfo.State -eq "Failed")
-                {
-                    $result  = $Threads[$idx].InvocationStateInfo.Reason
-                    Write-Error "Set Item: $($SetParamObj.Value) Exception: $result"
-                }
-                else
-                { 
-                    $result = $Threads[$idx].EndInvoke($Jobs[$idx])
-                }
-                $ts = (New-TimeSpan -Start $timer[$idx] -End (Get-Date))
-                if($Measure)
-                {
-                    new-object psobject -Property @{
-                        TimeSpan = $ts
-                        Output = $result
-                        SetItem = $SetParamObj.Value}
-                }
-                else
-                {
-                    $result
-                }
-                $Jobs[$idx] = $null
-                $JobsLeft-- #one less left
-                write-verbose "Completed: $($SetParamObj.Value) in $ts"
-                write-progress -Activity "Processing Set" -Status "$JobsLeft jobs left" -PercentComplete (($length-$jobsleft)/$length*100)
-            }
-            If(($Count -lt $Length) -and ($Jobs[$idx] -eq $null)) #add job if there is more to process
+            for($idx = 0; $idx -lt ($ThreadCount-1) ; $idx++)
             {
-                write-verbose "starting: $($Set[$Count])"
-                $timer[$idx] = get-date
-                $Threads[$idx].Commands.Commands[0].Parameters.Remove($SetParamObj) | Out-Null #check for success?
-                $Threads[$idx].AddParameter($SetParam,$Set[$Count]) | Out-Null
-                $Jobs[$idx] = $Threads[$idx].BeginInvoke()
-                $Count++
+                $SetParamObj = $Threads[$idx].Commands.Commands[0].Parameters| Where-Object {$_.Name -eq $SetParam}
+                
+                If($Jobs[$idx].IsCompleted) #job ran ok, clear it out
+                {  
+                    $result = $null
+                    if($threads[$idx].InvocationStateInfo.State -eq "Failed")
+                    {
+                        $result  = $Threads[$idx].InvocationStateInfo.Reason
+                        Write-Error "Set Item: $($SetParamObj.Value) Exception: $result"
+                    }
+                    else
+                    { 
+                        $result = $Threads[$idx].EndInvoke($Jobs[$idx])
+                    }
+                    $ts = (New-TimeSpan -Start $timer[$idx] -End (Get-Date))
+                    if($Measure)
+                    {
+                        new-object psobject -Property @{
+                            TimeSpan = $ts
+                            Output = $result
+                            SetItem = $SetParamObj.Value}
+                    }
+                    else
+                    {
+                        $result
+                    }
+                    $Jobs[$idx] = $null
+                    $JobsLeft-- #one less left
+                    write-verbose "Completed: $($SetParamObj.Value) in $ts"
+                    write-progress -Activity "Processing Set" -Status "$JobsLeft jobs left" -PercentComplete (($length-$jobsleft)/$length*100)
+                }
+                If(($Count -lt $Length) -and ($Jobs[$idx] -eq $null)) #add job if there is more to process
+                {
+                    write-verbose "starting: $($Set[$Count])"
+                    $timer[$idx] = get-date
+                    $Threads[$idx].Commands.Commands[0].Parameters.Remove($SetParamObj) | Out-Null #check for success?
+                    $Threads[$idx].AddParameter($SetParam,$Set[$Count]) | Out-Null
+                    $Jobs[$idx] = $Threads[$idx].BeginInvoke()
+                    $Count++
+                }
             }
-        }
 
+        }
+    }
+
+    End
+    {
+        $Threads | ForEach-Object{$_.runspace.close();$_.Dispose()}
     }
 }
-End
-{
-    $Threads | ForEach-Object{$_.runspace.close();$_.Dispose()}
-}
-}
 
-Export-ModuleMember -Function Invoke-Async
+#Export-ModuleMember -Function Invoke-Async
