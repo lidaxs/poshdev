f89769918e5ab95327a0d70199926286898129e3
ɦɐɹɹў <ADM-BouweH01@antoniuszorggroep.local>
2018-04-20 09:08:56 +0200

    version 1.0.1.2 aliases not working as expected when using pipeline

--- Update-HiXClient.ps1
+++ Update-HiXClient.ps1
@@ -1,4 +1,8 @@
 <#
+	version 1.0.1.2
+	aliases not working as expected when using pipeline and piping different types of objects
+	added if($Computer.Name){$Computer=$Computer.Name} in processblock
+
 	version 1.0.1.1
 	test connectivity now with wmi
 
@@ -161,6 +165,8 @@ function Update-Hixclient{
 		# Loop through collection
 		ForEach($Computer in $ClientName){
 
+			if($Computer.Name){$Computer=$Computer.Name}
+			
 			# Test connectivity
 			if ((Get-WmiObject -Query "Select * From Win32_PingStatus Where (Address='$Computer') and timeout=1000").StatusCode -eq 0)
 			{
