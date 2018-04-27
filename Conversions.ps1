c306687a2003d45cf654354d41cdcd9c92146080
compuser <compuser@N1205175.antoniuszorggroep.local>
2018-04-27 20:36:17 +0200

    1.0.3.0 added ConvertFrom-ErrorRecord

--- Conversions.ps1
+++ Conversions.ps1
@@ -1,4 +1,7 @@
 <#
+    version 1.0.3.0
+    added ConvertFrom-ErrorRecord
+    
     version 1.0.2.0
     added ToPSCustom
 
@@ -191,4 +194,38 @@ function ToPSCustom {
     
     end {
     }
+}
+
+function ConvertFrom-ErrorRecord
+{
+  [CmdletBinding(DefaultParameterSetName="ErrorRecord")]
+  param
+  (
+    [Management.Automation.ErrorRecord]
+    [Parameter(Mandatory,ValueFromPipeline,ParameterSetName="ErrorRecord", Position=0)]
+    $Record,
+    
+    [Object]
+    [Parameter(Mandatory,ValueFromPipeline,ParameterSetName="Unknown", Position=0)]
+    $Alien
+  )
+  
+  process
+  {
+    if ($PSCmdlet.ParameterSetName -eq 'ErrorRecord')
+    {
+      [PSCustomObject]@{
+        Exception = $Record.Exception.Message
+        Reason    = $Record.CategoryInfo.Reason
+        Target    = $Record.CategoryInfo.TargetName
+        Script    = $Record.InvocationInfo.ScriptName
+        Line      = $Record.InvocationInfo.ScriptLineNumber
+        Column    = $Record.InvocationInfo.OffsetInLine
+      }
+    }
+    else
+    {
+      Write-Warning "$Alien"
+    } 
+  }
 }
